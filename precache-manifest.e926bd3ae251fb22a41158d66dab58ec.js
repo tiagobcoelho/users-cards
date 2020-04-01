3a91aa176032b8b315e4b5422afbf100da8795a3
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-04-01 10:15:03 +0100

    Updates

---
+++ precache-manifest.e926bd3ae251fb22a41158d66dab58ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8175992a66cf9f4d99e0b96c09dc6421",
+    "url": "/user-cards/index.html"
+  },
+  {
+    "revision": "c2a803556abaa2953776",
+    "url": "/user-cards/static/css/main.37b9908d.chunk.css"
+  },
+  {
+    "revision": "2ed46c068a35d43a2e68",
+    "url": "/user-cards/static/js/2.d8d887f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-cards/static/js/2.d8d887f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a803556abaa2953776",
+    "url": "/user-cards/static/js/main.291f011e.chunk.js"
+  },
+  {
+    "revision": "059717206bcaa3e154d9",
+    "url": "/user-cards/static/js/runtime-main.d3d7324f.js"
+  }
+]);
